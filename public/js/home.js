77a521011bee8ca6ac937f262cc8452fc8cd641d
kaouech mouheb <mouhebkaouach@hotmail.fr>
2020-06-16 18:00:54 +0200

    modification du code javascript

--- public/js/home.js
+++ public/js/home.js
@@ -10,39 +10,41 @@ const fetchCameras = fetch(url)
     myFuction(element.name, element.imageUrl, element.name, element.name, element.description, element.price)
     });
   });
-
+  const element = document.getElementById("row-test");
+  let bloc = "";
   const myFuction = (titre, src, alt, title, description, price)=>{
-    let element = document.getElementById("row-test"),
-        bloc = document.createElement("div");
-        //insersation d'une classBOOTSTRAP abec la function classLIST.add()
-    bloc.classList.add("col-md-6", "animation-img");    
-    let insersationDiv = element.appendChild(bloc);
-        //insersation d'un h2 avec la methode appenchild
-        insersationtitre = (()=>{
-          let t = document.createElement("h2");
-          t.innerText = titre;
-          bloc.appendChild(t);
-        })(),
-        //insersation d'une image avec la methode appenchild
-        insersationImg = (()=>{
-          let x = document.createElement('img');
-              x.setAttribute("src", src);
-              x.setAttribute("alt", alt);
-              x.setAttribute("title", title);
-              bloc.appendChild(x);
-        })(),
-        //insersation de la description avec la methode appenchild
-        insersationDescrition = (()=>{
-          let y = document.createElement("p");
-              y.innerText = description;
-              bloc.appendChild(y);
-
-        })();
-        //Insersation du prix avec la methode appenchild
-        insersationPrice = (()=>{
-          let z = document.createElement("p");
-              z.innerText = price;
-              bloc.appendChild(z);
-        })();
+    bloc = document.createElement("div");
+    element.appendChild(bloc);
+    bloc.classList.add("col-lg-6", "animation-img")
+    insersationtitre(titre); 
+    insersationImg(src, alt, title);
+    insersationDescrition(description);
+    insersationPrice(price);
   };
+    //insersation d'un h2 avec la methode appenchild
+  const insersationtitre = (titre) =>{
+      let t = document.createElement("h2");
+      t.innerText = titre;
+      bloc.appendChild(t);
+    };
+       //insersation d'une image avec la methode appenchild
+  const insersationImg = (src, alt, title)=>{
+        const x = document.createElement('img');
+            x.setAttribute("src", src);
+            x.setAttribute("alt", alt);
+            x.setAttribute("title", title);
+            bloc.appendChild(x);
+      };
+       //insersation de la description avec la methode appenchild
+  const insersationDescrition = (description)=>{
+        let y = document.createElement("p");
+            y.innerText = description;
+            bloc.appendChild(y);
+      };
+       //Insersation du prix avec la methode appenchild
+  const insersationPrice = (price)=>{
+        let z = document.createElement("p");
+            z.innerText = price;
+            bloc.appendChild(z);
+      };
       

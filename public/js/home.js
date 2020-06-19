7cef2cf0cc80d317202e3b7bc1fb6d33af54dc74
kaouech mouheb <mouhebkaouach@hotmail.fr>
2020-06-19 21:12:51 +0200

    vérification et modification du code JS, nouvelle version du code plus pratique, les produits sans implémanter dans le contenu de la page en html avec des appels en javascript

--- public/js/home.js
+++ public/js/home.js
@@ -8,34 +8,73 @@ const fetchCameras = fetch(url)
     data.forEach((element) => {
     myFuction(element.name, element.imageUrl, element.name, element.name, element.description, element.price)
     });
-    ProductJs(zurss, zurssDescription, data[0].name, data[0].imageUrl, data[0].description, data[0].price, data[0].lenses[0], data[0].lenses[1]);
-    ProductJs(hirschProduct, hirschDescription, data[1].name, data[1].imageUrl, data[1].description, data[1].price, data[1].lenses[0], data[1].lenses[1]);
-    ProductJs(Franck, FranckDescription, data[2].name, data[2].imageUrl, data[2].description, data[2].price, data[2].lenses[0], data[2].lenses[1]);
-    ProductJs(Kuros, KurosDescription, data[3].name, data[3].imageUrl, data[3].description, data[3].price, data[3].lenses[0], data[3].lenses[1]);
-    ProductJs(Katatone, KatatoneDescription, data[4].name, data[4].imageUrl, data[4].description, data[4].price, data[4].lenses[0], data[4].lenses[1]);
     attendreAsync();
+   product(zurssH1, zurssImg, zurssp1, zurssP2, zurssOp1, zurssOp2, data[0].name, data[0].imageUrl, data[0].price, data[0].description, data[0].lenses[0], data[0].lenses[1]);
+
+   product(hirshH1, hirshImg, hirshp1, hirshP2, hirshOp1, hirshOp2, data[1].name, data[1].imageUrl, data[1].price, data[1].description,  data[1].lenses[0], data[1].lenses[1]);
+
+   product(FranckH1, FranckImg, Franckp1, FranckP2, FranckOp1, FranckOp2, data[2].name, data[2].imageUrl, data[2].price, data[2].description,  data[2].lenses[0], data[2].lenses[1]);
+
+   product(KurosH1, KurosImg, KurosP1, KurosP2, KurosOp1, KurosOp2, data[3].name, data[3].imageUrl, data[3].price, data[3].description,  data[3].lenses[0], data[3].lenses[1]);
+
+   product(KatatoneH1, KatatoneImg, KatatoneP1, KatatoneP2, KatatoneOp1, KatatoneOp2, data[4].name, data[4].imageUrl, data[4].price, data[4].description,  data[4].lenses[0], data[4].lenses[1]);
   });
-  /*creation d'une function asynchrone qui permet d'attendre le telechargement de l'ensemble de code avant de s'exucuter*/
+
+  /*Produit zurss parametre*/
+  let zurssH1 = "titre1",
+      zurssImg = "srcImage1",
+      zurssp1 = "price1",
+      zurssP2 = "description1",
+      zurssOp1 = "first1",
+      zurssOp2 = "second1";
+  /*Produit hirsh parametre */
+  let hirshH1 = "titre2",
+      hirshImg = "srcImage2",
+      hirshp1 = "price2",
+      hirshP2 = "description2",
+      hirshOp1 = "first2",
+      hirshOp2 = "second2";
+  /*Produit Franck parametre */
+  let FranckH1 = "titre3",
+      FranckImg = "srcImage3",
+      Franckp1 = "price3",
+      FranckP2 = "description3",
+      FranckOp1 = "first3",
+      FranckOp2 = "second3";
+  /*Produit Kuros parametre */
+  let KurosH1 = "titre4",
+      KurosImg = "srcImage4",
+      KurosP1 = "price4",
+      KurosP2 = "description4",
+      KurosOp1 = "first4",
+      KurosOp2 = "second4";
+  /*Produit Kuros parametre */
+  let KatatoneH1 = "titre5",
+      KatatoneImg = "srcImage5",
+      KatatoneP1 = "price5",
+      KatatoneP2 = "description5",
+      KatatoneOp1 = "first5",
+      KatatoneOp2 = "second5"; 
+  
+  /*creation d'une function asynchrone qui permet d'attendre le  de l'ensemble de code avant de s'exucuter*/
   async function attendreAsync() {
-    document.querySelectorAll("div > span")[0].innerHTML = `<strong><a href="zurss-50S.html" class="">Détail</stong></a>`;
-    document.querySelectorAll("div > span")[1].innerHTML = `<strong><a href="Hirsch-400DTS.html" class="">Détail</stong></a>`;
-    document.querySelectorAll("div > span")[2].innerHTML = `<strong><a href="Franck-JS-105.html" class="">Détail</stong></a>`;
-    document.querySelectorAll("div > span")[3].innerHTML = `<strong><a href="Kuros-TTS.html" class="">Détail</stong></a>`;
-    document.querySelectorAll("div > span")[4].innerHTML = `<strong><a href="Katatone.html" class="">Détail</stong></a>`;
+    try{
+      let lien1 = document.querySelectorAll("div > span")[0];
+        lien1 !== undefined ? lien1.innerHTML = `<strong><a href="zurss-50S.html" class="">Détail</stong></a>`:true;
+    let lien2 = document.querySelectorAll("div > span")[1];
+        lien2 !== undefined ? lien2.innerHTML = `<strong><a href="Hirsch-400DTS.html" class="">Détail</stong></a>`:true;
+    let lien3 = document.querySelectorAll("div > span")[2];
+        lien3 !== undefined ? lien3.innerHTML = `<strong><a href="Franck-JS-105.html" class="">Détail</stong></a>`:true;
+    let lien4 = document.querySelectorAll("div > span")[3];
+        lien4 !== undefined ? lien4.innerHTML = `<strong><a href="Kuros-TTS.html" class="">Détail</stong></a>`:true;
+    let lien5 = document.querySelectorAll("div > span")[4];
+        lien5 !== undefined ? lien5.innerHTML = `<strong><a href="Katatone.html" class="">Détail</stong></a>`:true;
+    }
+    catch{
+      console.error(error)
+    }
   };
-
-  /*creation des parametres pour passer les differents id dans la functions ProductJs*/
-  const zurss = "product";
-  const zurssDescription = "product-description";
-  const hirschProduct = "second-product";
-  const hirschDescription = "second-product_description";
-  const Franck = "third-product";
-  const FranckDescription = "third-product_description";
-  const Kuros = "foorth-product";
-  const KurosDescription = "foorth-product_description";
-  const Katatone = "firth-product";
-  const KatatoneDescription = "firth-product_description";
-
+   
 /*creation d'une function permet d'afficher les informations des produits dans le contenu de la page index.html*/
 const myFuction = (titre, src, alt, title, description, price)=>{
   const element = document.getElementById("row-test");
@@ -61,40 +100,27 @@ const myFuction = (titre, src, alt, title, description, price)=>{
       spanButton.classList.add("span-button");
       bloc.appendChild(spanButton);  
 };
+  
+/*Insesation des données produits dans la page produit*/
+const product = (camTit, camImg, camPri, camDes, camOp1, camOp2, titreProduct, image, price, description, firstOption, secondtOption) =>{
+  try{
+    let titre = document.getElementById(camTit);
+    titre !== null ? titre.innerText = titreProduct: true;
+let imageSize = document.getElementById(camImg);
+    imageSize !== null ? imageSize.setAttribute("src", image):true;
+    imageSize !== null ? imageSize.setAttribute("width", "100%"):true;
+let prix = document.getElementById(camPri);
+    prix !== null ? prix.innerText = `Prix ${price} euros`: true;
+let descr = document.getElementById(camDes);
+    descr !== null ? descr.innerHTML = `<strong>Description Produit</strong><br> ${description}`:true;
+let optionOne = document.getElementById(camOp1);
+    optionOne !== null ? optionOne.innerText = firstOption:true; 
+let optionTwo = document.getElementById(camOp2);
+    optionTwo !== null ? optionTwo.innerText = secondtOption:true;
+  }
+  catch{
+    console.error(error)
+  }
+}
 
-/*creation d'une functions qui permet de repeter les memes instructions a toutes les pages produits*/
-const ProductJs = (param1, param2, titre, image, description, prix, lenses1, lenses2) =>{
-  const product1 = document.getElementById(param1);
-  const node = document.createElement("div");
-  product1 !== null ? product1.appendChild(node): false;
-  const titreNode = document.createElement("h1");
-  titreNode.innerText = titre;
-  node.appendChild(titreNode)
-  const imgNode = document.createElement("img");
-        imgNode.setAttribute("src", image);
-        imgNode.setAttribute("alt", titre);
-        imgNode.setAttribute("title", titre);
-        imgNode.setAttribute("width", "100%");
-  node.appendChild(imgNode);
-  const price = document.createElement("p");
-  price.innerHTML =`Pour seulement <strong>${prix} Euros</strong>`;
-  node.appendChild(price);
-  price.classList.add("price-product");
-  const descriptionJs = document.getElementById(param2);
-  const nodeP = document.createElement("p");
-  nodeP.innerText = description;
-  nodeP.classList.add("description-product");
-  const nodeLabel = document.createElement("label");
-  nodeLabel.innerHTML =` Choisissey les lentilles`;
-  nodeLabel.classList.add("label-option");
-  const nodeSelect = document.createElement("select");
-  nodeSelect.classList.add("select-option");
-  const nodeOption1 = document.createElement("option");
-  const nodeOption2 = document.createElement("option");
-  nodeOption1.innerHTML =lenses1;
-  nodeOption2.innerHTML =lenses2;
-  const buttonPanier = document.createElement("button");
-  buttonPanier.classList.add("button")
-  buttonPanier.innerText ="Ajouter au panier";
-  descriptionJs !== null ? descriptionJs.appendChild(nodeP) && descriptionJs.appendChild(nodeLabel) && descriptionJs.appendChild(nodeSelect) && nodeSelect.appendChild(nodeOption1) && nodeSelect.appendChild(nodeOption2) && descriptionJs.appendChild(buttonPanier): false;
-};
+/* Local storage*/

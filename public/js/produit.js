54878a670fd1b1dd29f082595a4451b49aee7f8c
kaouech mouheb <mouhebkaouach@hotmail.fr>
2020-06-22 18:18:43 +0200

    creation de la page produit

--- public/js/produit.js
+++ public/js/produit.js
@@ -1,144 +1,64 @@
-const createElement = element => { return document.createElement(element) }
-
-const append = (parent, el) => { return parent.appendChild(el) }
-
-// recuperation de l'id dans l'url
-const urlParams = new URLSearchParams(window.location.search)
-let idTeddies = urlParams.get("id")
-
-// la div où seront append les produits
-let choosenProduct = document.querySelector("#choosenProduct")
-
-// initialisation de la variable teddy qui va contenir les data
-let teddy = null;
-
-// init de la cart
-let cart = [];
 
 
-const addTocart = () => {
-    const teddyAdd = {
-        name: teddy.name,
-        image: teddy.imageUrl,
-        color: teddy.colors[0],
-        price: teddy.price,
-    }
-    cart = [...cart, teddyAdd];
-
-    let addedProduct = localStorage.setItem("cart", JSON.stringify(cart))
-}
 
 
-// je verifie si il y a quelque chose dans le local storage :
-const checkStorage = localStorage.getItem("cart")
-const checkStorageParse = JSON.parse(localStorage.getItem("cart"))
+/** recuperation du url de la page sous forme d'un objet*/
+const urlLocation = new URLSearchParams(window.location.search);
 
-// si il y a quelque chose dedans j'ajoute dans cart le contenu de localStorage
-if (checkStorage) {
-    cart = [...checkStorageParse]
-}
+/**recuperation de l'id  */
+const camerasId = urlLocation.get("id");
 
-fetch("http://localhost:3000/api/cameras/" + idTeddies)
+
+/** marquage de la section qui va reçevoir les produits*/
+const newProduct = document.querySelector("#new-product");
+
+fetch("http://localhost:3000/api/cameras/" + camerasId)
 
     .then(response => response.json())
-    .then(function (data) {
-        teddy = data
+    .then((data) => {
+       let cameras = data;
+        /**creation de la section qui permet d'afficher le produit */
+        const divImage = document.createElement("div");
+        divImage.classList.add("col-md-6", "col-sm-12");
+        const imgImage = document.createElement("img");
+        const divPrice = document.createElement("div");
 
-        // CREATION DES DIV QUI VONT CONTENIR LES ELEMENTS
+        imgImage.src = cameras.imageUrl;
+        divPrice.innerHTML =`<strong>Prix ${cameras.price / 100} Euros</strong>`;
 
-        const divCol4 = createElement("div")
-        divCol4.classList.add("col-4")
+        newProduct.appendChild(divImage);
+        divImage.appendChild(imgImage);
+        divImage.appendChild(divPrice);
 
-        const divCol8 = createElement("div")
-        divCol8.classList.add("col-8")
-        divCol8.classList.add("d-flex")
-        divCol8.classList.add("flex-column")
+        /**creation de la section qui permet d'afficher la description et les options */
 
-        // CREATION DES ELEMENTS
-        let teddyName = createElement("h2")
-        teddyPrice = createElement("span")
-        teddyDescription = createElement("p")
-        teddyImage = createElement("img")
-        teddyLabel = createElement("label")
-        teddySelect = createElement("select")
-        returnLink = createElement("a")
-        labelQt = createElement("label")
-        qtSelect = createElement("select")
-        addButton = createElement("button")
+        const divDescription = document.createElement("div");
+        divDescription.classList.add("col-md-6", "col-sm-12");
+        const nameProduct = document.createElement("h1");
+        const pDescription = document.createElement("p");
+        const labelOption = document.createElement("label");
+        const selecOption = document.createElement("select");
+        const addButton = document.createElement("button");
+        nameProduct.innerText = cameras.name;
+        pDescription.innerHTML =`<strong>Description Produit <br></strong> ${cameras.description}.`;
+        addButton.innerText=`Ajouter au panier`
+        
+        cameras.lenses.forEach(el => {
+            let option = document.createElement("option")
+            let lentilles = el;
+            option.setAttribute("value", lentilles)
+            option.innerHTML = lentilles;
+            selecOption.appendChild(option);
+        });
 
-        // INNER
-        teddyName.innerHTML = teddy.name
-        teddyPrice.innerHTML = "Price : " + " " + teddy.price / 100 + " $"
-        teddyDescription.innerHTML = "Description :" + " " + teddy.description
-        teddyImage.src = teddy.imageUrl
-        teddyLabel.innerHTML = "Colors : "
-        returnLink.innerHTML = "Previous"
-        labelQt.innerHTML = "Quantity  : "
-        addButton.innerHTML = "Add to cart"
-        // MODIF DES CLASSES
-        teddyName.classList.add("teddyName")
-        teddyPrice.classList.add("teddyPrice")
-        teddyDescription.classList.add("teddyDescription")
-        teddyImage.classList.add("teddyImg")
-        returnLink.classList.add("returnLink")
-        qtSelect.classList.add("qtSelect")
-        addButton.classList.add("btn")
-        addButton.classList.add("btn-outline-dark")
-        addButton.classList.add("add-cart")
-        // AJOUT ATTRIBUTS
-        teddyLabel.setAttribute("for", "color")
-        teddySelect.setAttribute("id", "liste")
-        returnLink.setAttribute("href", "index.html")
-        labelQt.setAttribute("for", "qt")
-        qtSelect.setAttribute("id", "qt")
+        newProduct.appendChild(divDescription);
+        divDescription.appendChild(pDescription);
+        divDescription.appendChild(labelOption);
+        divDescription.appendChild(selecOption);
+        divDescription.appendChild(nameProduct);
+        divDescription.appendChild(addButton);
 
+      
+    });
 
-        for (let i = 0; i < teddy.lenses.length; i++) {
-
-            var option = createElement("option")
-            var teddyColors = teddy.lenses[i];
-            option.setAttribute("value", teddyColors)
-            option.innerHTML = teddyColors
-            append(teddySelect, option)
-
-        }
-
-        function selectQuantity() {
-
-            for (let i = 1; i < 6; i++) {
-                let qtChoice = createElement("option");
-                qtChoice.innerHTML = i
-                qtChoice.value = i
-                qtChoice.setAttribute("value", "quantity")
-                append(qtSelect, qtChoice)
-            }
-        }
-        selectQuantity()
-
-        // APPEND DES ELEMENTS CREE A LA DIV QUI DOIT LES ACCUEILLIR
-        append(choosenProduct, divCol4)
-        append(divCol4, teddyImage)
-        append(divCol4, returnLink)
-
-        // APPEND COL 8
-        append(choosenProduct, divCol8)
-        append(divCol8, teddyName)
-        append(divCol8, teddyDescription)
-        append(divCol8, teddyPrice)
-        append(divCol8, teddyLabel)
-        append(teddyLabel, teddySelect)
-        append(divCol8, labelQt)
-        append(divCol8, qtSelect)
-        append(divCol8, addButton)
-
-        // FUNCTION FOR ADDING CONTENT TO CART
-
-        addButton.addEventListener("click", () => {
-
-            addTocart()
-
-        })
-
-
-    })
 
